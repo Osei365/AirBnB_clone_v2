6c34ff318d385b8caf8fccb364c4ada195e5a2dc
osei <williamsabodunrin@gmail.com>
2023-10-05 18:51:18 +0000

    pack web static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""create the archive file."""
+
+from fabric.api import *
+from datetime import datetime as dt
+
+def do_pack():
+    """function to archive web static."""
+    local('mkdir -p versions')
+    the_time = dt.now().strftime('%Y%m%d%H%M%S')
+    filename = 'versions/web_static_{}.tgz'.format(the_time)
+    comd = local('tar -cvzf {} web_static'.format(filename))
+    if comd is not None:
+        return filename
+    else:
+        return None
