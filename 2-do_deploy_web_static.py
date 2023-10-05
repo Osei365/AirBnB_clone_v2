45b473190ea5d949cb461aae6bf15ba46659ce0a
Osei <williamsabodunrin@gmail.com>
2023-10-05 21:59:49 +0000

    move wen static to web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """send web static files to remotes server."""
-
+import os
 from fabric.api import *
 
 env.hosts = ['18.204.5.64', '52.21.57.114']
@@ -8,20 +8,28 @@ env.hosts = ['18.204.5.64', '52.21.57.114']
 
 def do_deploy(archive_path):
     """define a function to handle web static."""
-    try:
-        fn = archive_path.split('/')[1]
-        dest_file = '/tmp/' + fn
-        put(archive_path, dest_file)
-        ef = fn.split('.')[0] # end file
-        run('mkdir -p /data/web_static/releases/{}/'.format(ef))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(fn, ef))
-        run('rm /tmp/{}'.format(fn))
-        path = '/data/web_static/releases/{}/'.format(ef)
-        run('mv {}web_static/* {}'.format(path, path))
-        run('rm -rf {}web_static'.format(path))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {} /data/web_static/current'.format(path))
-        print('New version deployed!')
-        return True
-    except Exception:
+    if os.path.isfile(archive_path) is False:
         return False
+    fn = archive_path.split('/')[1]
+    dest_file = '/tmp/' + fn
+    ef = fn.split('.')[0] # end file
+    path = '/data/web_static/releases/{}/'.format(ef)
+    
+    if put(archive_path, dest_file).failed:
+        return False
+    if run('mkdir -p {}'.format(path)).failed:
+        return False
+    if run('tar -xzf /tmp/{} -C {}'.format(fn, path)).failed:
+        return False
+    if run('rm /tmp/{}'.format(fn)).failed:
+        return False
+    if run('mv {}web_static/* {}'.format(path, path)).failed:
+        return False
+    if run('rm -rf {}web_static'.format(path)).failed:
+        return False
+    if run('rm -rf /data/web_static/current').failed:
+        return False
+    if run('ln -s {} /data/web_static/current'.format(path)).failed:
+        return False
+    print('New version deployed!')
+    return True
