532980a760d87c91c0f86557bee4f3723c2470f8
Osei <williamsabodunrin@gmail.com>
2023-10-21 00:22:49 -0700

    c flask routing

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""defining a flask app."""
+
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    text = text.replace('_', ' ')
+    return 'C {}'.format(escape(text))
+
+
+if __name__ == "__main__":
+    """command line"""
+    app.run('0.0.0.0', 5000)
