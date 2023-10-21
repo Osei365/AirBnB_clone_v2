5ca6e4927a2654e2af8723ed18711d4c31b36e79
Osei <williamsabodunrin@gmail.com>
2023-10-21 00:13:28 -0700

    hbnb route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""defining a flask app."""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    return "HBNB"
+
+if __name__ == "__main__":
+    """command line"""
+    app.run('0.0.0.0', 5000)
